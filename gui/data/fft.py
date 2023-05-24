1954da52c1ac378c0589f7225e27ffee1659082a
opto comb <qleap44@gmail.com>
2023-05-24 15:00:30 +0900

    テストデータ解析：単振り子のFFT

---
+++ gui/data/fft.py
@@ -0,0 +1,40 @@
+from numpy.fft import rfft, rfftfreq
+from csv import reader
+import numpy as np
+import matplotlib.pyplot as plt
+
+CAMERA_SECONDS_PER_FRAME = 0.033
+CAMERA_SECONDS_PER_FRAME_EPSILON = 0.005
+
+x_coodinates = []
+
+with open('./gui/simple-pendulum-fetch.csv', "r") as f:
+    reader_object = reader(f)
+    old_t = 190.8394623
+    old_x = 196.5
+    for t, x1 in reader_object:
+        t = float(t)
+        x1 = float(x1)
+
+        # 足りないデータは線形補間
+        while t > (old_t + CAMERA_SECONDS_PER_FRAME + CAMERA_SECONDS_PER_FRAME_EPSILON):
+            old_t += CAMERA_SECONDS_PER_FRAME
+            old_x = x1 + (x1-old_x)/((t-old_t)/CAMERA_SECONDS_PER_FRAME)
+            x_coodinates.append(old_x)
+
+        x_coodinates.append(x1)
+        old_t = t
+        old_x = x1
+    f.close()
+
+# x_coodinates = np.array(x_coodinates)
+fftResult = rfft(x_coodinates)
+n = len(x_coodinates)
+freq = rfftfreq(n, d=CAMERA_SECONDS_PER_FRAME)
+
+# 0Hz 成分が極端に大きく出たため、そこだけ取り除いた。
+plt.plot(freq[1:], fftResult[1:])
+plt.xlabel("frequency / Hz")
+plt.ylabel("amplitude / px")
+plt.grid()
+plt.show()
--- gui/fft.py
+++
@@ -1,8 +0,0 @@
-from numpy.fft import fft, fftfreq
-from csv import reader
-import matplotlib.pyplot as plt
-
-with open(self.csvfile, "w", newline = '') as f:
-                writer_object = writer(f)
-                writer_object.writerow(columns_name)
-                f.close()
